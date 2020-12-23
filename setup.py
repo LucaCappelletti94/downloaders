f5ef7349eefbc77cd3e905ecf437f416bf762522
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-12-23 19:43:19 +0100

    Updated requirements

--- setup.py
+++ setup.py
@@ -60,7 +60,8 @@ setup(
     tests_require=test_deps,
     # Add here the package dependencies
     install_requires=[
-        "tqdm"
+        "tqdm",
+        "pandas"
     ],
     extras_require=extras,
 )
